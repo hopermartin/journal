7abaa3354098e0db22850d0e1923e01f5de1ce80
Hope Martin <hope@hopes-mbp.myfiosgateway.com>
2022-02-07 20:27:52 -0600

    Adding pages and entry model

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -1,4 +1,22 @@
 class EntriesController < ApplicationController
+  def show
+    @entry = Entry.find(params[:id])
+  end
+
+  def index
+    @entries = Entry.all
+  end
+
   def new
+  end
+
+  def create
+    @entry = Entry.new(entry_params)
+    @entry.save
+    redirect_to @entry
+  end
+
+  def entry_params
+    params.require(:entry).permit(:title, :text)
   end
 end
